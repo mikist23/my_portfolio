51dd2453e49c17cd75d09e1bfebfd6d72b8d5a9f
@Mikist <mikeochango3@gmail.com>
2024-12-28 23:09:31 +0300

    update on  hero section on deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
-import Hero from "./pages/Hero.Jsx";
+import Hero from "./pages/Hero";
 import Portfolio from "./pages/Portfolio";
 import Stack from "./pages/Stack";
 
